d3c725b322fddc1c742b83d8536bf722bd6cf098
Johnathan Zhou <johnathanz@berkeley.edu>
2018-09-21 18:24:44 -0700

    done

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,7 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map { |i| i.to_i + 2}.select { |num| num % 2 == 0 && num < 10 }.uniq.sum
+
   end
 end
